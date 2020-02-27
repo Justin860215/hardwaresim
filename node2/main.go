4991bd51979e8461f5d267b4e58dc1f5708600a7
Vimalakirti <jade@LAPTOP-TN0HDTFH.localdomain>
2020-02-27 14:18:05 +0800

    demo

--- node2/main.go
+++ node2/main.go
@@ -37,43 +37,45 @@ func main(){
 	blockchain, _ := brizochain.NewBrizoChain()
 	conn, _ := net.Dial("tcp", "172.20.10.8:8081")
 	puf := hardwaresim.CreateTestPuf()
-	go func() { // goroutine1
-		consolescanner := bufio.NewScanner(os.Stdin)
-		// by default, bufio.Scanner scans newline-separated lines
-		for consolescanner.Scan() {
-			original := consolescanner.Text()
-			content := []byte(original)
-			encryptContent, key, _ := hardwaresim.Encrypt(content)
-			
-			hashString, _ := hardwaresim.HashString(encryptContent)
-			hash := hardwaresim.HashStringToHash(hashString)
-			
-			
-			sig, pubKey, _ := puf.Sign(hashString)
-
-			// todo: ENCODE 4 of them together
-			res := &Block1{
-				E: encryptContent,
-				K: key,
-				H: hash,
-				P: pubKey,
-				S: sig,
-			}
-			encode := res.Serialize()
-			encodeString := fmt.Sprintf("%x", encode[:])
-			_ = blockchain.WriteByHashKey(hashString, encodeString)
-			fmt.Println(hashString)
-			conn.Write(append(hash,0x0d))
-		}
+	for {
+		go func() { // goroutine1
+			consolescanner := bufio.NewScanner(os.Stdin)
+			// by default, bufio.Scanner scans newline-separated lines
+			for consolescanner.Scan() {
+				original := consolescanner.Text()
+				content := []byte(original)
+				encryptContent, key, _ := hardwaresim.Encrypt(content)
+				
+				hashString, _ := hardwaresim.HashString(encryptContent)
+				hash := hardwaresim.HashStringToHash(hashString)
+				
+				
+				sig, pubKey, _ := puf.Sign(hashString)
 	
-		// check once at the end to see if any errors
-		// were encountered (the Scan() method will
-		// return false as soon as an error is encountered) 
-		if err := consolescanner.Err(); err != nil {
-			 fmt.Println(err)
-			 os.Exit(1)
-		}
-	}()
+				// todo: ENCODE 4 of them together
+				res := &Block1{
+					E: encryptContent,
+					K: key,
+					H: hash,
+					P: pubKey,
+					S: sig,
+				}
+				encode := res.Serialize()
+				encodeString := fmt.Sprintf("%x", encode[:])
+				_ = blockchain.WriteByHashKey(hashString, encodeString)
+				fmt.Println(hashString)
+				conn.Write(append(hash,0x0d))
+			}
+		
+			// check once at the end to see if any errors
+			// were encountered (the Scan() method will
+			// return false as soon as an error is encountered) 
+			if err := consolescanner.Err(); err != nil {
+				 fmt.Println(err)
+				 os.Exit(1)
+			}
+		}()
+	}
 }
 
 
